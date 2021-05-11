f35666a85d33728ea172ba6482290efae59af019
chrisorisawayi <chris.orisawayi@hmcts.net>
2021-05-11 11:37:45 +0100

    updated tfenv install to invoke use version after install

--- scripts/tfenv-install-terraform.sh
+++ scripts/tfenv-install-terraform.sh
@@ -9,6 +9,6 @@ sudo ln -s ~/.tfenv/bin/* /usr/local/bin
 # Install and invoke use
 echo "Installing Terraform based on version detected in .terraform-version file"
 tfenv install | tee -a tfenv_install.log
-# cat tfenv_install.log | grep -i 'tfenv use' | cut -d "'" -f 2 > tfenv_use.sh
-# chmod a+x tfenv_use.sh
-# ./tfenv_use.sh
+cat tfenv_install.log | grep -i 'tfenv use' | cut -d "'" -f 2 > tfenv_use.sh
+chmod a+x tfenv_use.sh
+./tfenv_use.sh
