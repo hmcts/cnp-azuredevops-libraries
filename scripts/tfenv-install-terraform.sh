b845201eaad604ca5e7a4bf5b77f5a9e2fa2965c
chrisorisawayi <chris.orisawayi@hmcts.net>
2021-05-11 11:23:01 +0100

    updated script to install terraform based on detected .terraform-version file

--- scripts/tfenv-install-terraform.sh
+++ scripts/tfenv-install-terraform.sh
@@ -6,9 +6,9 @@ git clone https://github.com/tfutils/tfenv.git ~/.tfenv
 sudo rm /usr/local/bin/terraform
 sudo ln -s ~/.tfenv/bin/* /usr/local/bin
 
-# run 'tfenv use version' as tfenv version > 1.0.2 currently does not auto switch to use installed version
-echo "Installing Terraform based on minimum required version detected"
-tfenv install min-required | tee -a tfenv_install.log
-cat tfenv_install.log | grep -i 'tfenv use' | cut -d "'" -f 2 > tfenv_use.sh
-chmod a+x tfenv_use.sh
-./tfenv_use.sh
+# Install and invoke use
+echo "Installing Terraform based on version detected in .terraform-version file"
+tfenv install | tee -a tfenv_install.log
+# cat tfenv_install.log | grep -i 'tfenv use' | cut -d "'" -f 2 > tfenv_use.sh
+# chmod a+x tfenv_use.sh
+# ./tfenv_use.sh
