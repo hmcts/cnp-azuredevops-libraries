3c8ad944b6b48186867d9a16a3fce841034b7cea
chrisorisawayi <chris.orisawayi@hmcts.net>
2021-05-14 16:33:40 +0100

    update to use bashrc

--- scripts/tfenv-install-terraform.sh
+++ scripts/tfenv-install-terraform.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -e
 
+if [ -d "~/.tfenv" ]; then
+  rm -rf ~/.tfenv/
+fi
+
 git clone -b v2.2.2 --single-branch https://github.com/tfutils/tfenv.git ~/.tfenv
 
 mkdir -p ~/.local/bin
 ln -s ~/.tfenv/bin/* ~/.local/bin
-echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.profile
-. ~/.profile
+echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.bashrc
+. ~/.bashrc
 which tfenv
-echo $PATH
 
 # Install and invoke use
 echo "Installing Terraform based on version detected in .terraform-version file"
@@ -16,4 +19,3 @@ tfenv install | tee -a tfenv_install.log
 cat tfenv_install.log | grep -i 'tfenv use' | cut -d "'" -f 2 > tfenv_use.sh
 chmod a+x tfenv_use.sh
 ./tfenv_use.sh
-terraform
