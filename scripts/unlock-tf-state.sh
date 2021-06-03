49fade675501e923fa71b834a1ca851ba8e096fb
Felix Eyetan <peyetan@yahoo.com>
2021-06-03 20:51:41 +0100

    WIP: test process argument variale

--- scripts/unlock-tf-state.sh
+++ scripts/unlock-tf-state.sh
@@ -13,8 +13,6 @@ state_file=$(echo $TERRAFORMSTATEFILE | sed -e 's/ /\\ /')
 echo "Checking lease state on: $state_file"
 leaseExist=$(az storage blob show --container-name $state_file_container_name --name $state_file --account-name $storage_account_name | jq -r '.properties.lease.state')
 
-echo "Current lease state: $leaseExist"
-
 if [ ${leaseExist} = "leased" ]; then
   echo "Releasing lock on state file"
   az storage blob lease break --blob-name $state_file --container-name $state_file_container_name --account-name $storage_account_name
