b53de3234786fc9f122cc6bd58b0cab3ecd32ecf
Felix Eyetan <peyetan@yahoo.com>
2021-06-03 19:58:24 +0100

    using environment variable instead

--- scripts/unlock-tf-state.sh
+++ scripts/unlock-tf-state.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 set -e
 
-terraform_state_file="$1"
-state_file_container_name=$2
-storage_account_name=$3
+state_file_container_name=$1
+storage_account_name=$2
 
-echo "State file: $TERRAFORMSTATEFILE"
-
-leaseExist=$(az storage blob show --container-name $state_file_container_name --name $terraform_state_file --account-name $storage_account_name | jq -r '.properties.lease.state')
+echo "Check state lock on: $TERRAFORMSTATEFILE"
+leaseExist=$(az storage blob show --container-name $state_file_container_name --name $TERRAFORMSTATEFILE --account-name $storage_account_name | jq -r '.properties.lease.state')
 
 echo "Current lease state: $leaseExist"
 
 if [ ${leaseExist} = "leased" ]; then
-az storage blob lease break --blob-name $terraform_state_file --container-name $state_file_container_name --account-name $storage_account_name
+az storage blob lease break --blob-name $TERRAFORMSTATEFILE --container-name $state_file_container_name --account-name $storage_account_name
 else
 exit 0
 fi
