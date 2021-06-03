375edbc5cc9d8554f9a60a0e672a94708daa998b
Felix Eyetan <peyetan@yahoo.com>
2021-06-03 20:46:17 +0100

    WIP: test process argument variale

--- scripts/unlock-tf-state.sh
+++ scripts/unlock-tf-state.sh
@@ -4,11 +4,13 @@ set -e
 state_file_container_name=$1
 storage_account_name=$2
 
-echo "Check lease state on: $TERRAFORMSTATEFILE"
 
-# The state file location has a space e.g. UK South, this confuses the process,
-# escaping the white space seems to cut it
+# The state file key has a space e.g. UK South, using it as the --name parameter without
+# escaping the white space makes the process see two inputs and treats every thing
+# after the space e.g. South/... as another argument
 state_file=$(echo TERRAFORMSTATEFILE | sed -e 's/ /\\ /')
+
+echo "Checking lease state on: $state_file"
 leaseExist=$(az storage blob show --container-name $state_file_container_name --name $state_file --account-name $storage_account_name | jq -r '.properties.lease.state')
 
 echo "Current lease state: $leaseExist"
@@ -17,5 +19,6 @@ if [ ${leaseExist} = "leased" ]; then
   echo "Releasing lock on state file"
   az storage blob lease break --blob-name $state_file --container-name $state_file_container_name --account-name $storage_account_name
 else
+  echo "No exiting lock on state file"
   exit 0
 fi
