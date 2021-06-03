a100439a5c199b99cc2a0edb4f2db5f3bde14141
Felix Eyetan <peyetan@yahoo.com>
2021-06-03 14:34:55 +0100

    Update to state file definition and enhanced unlock scripts to use additional variables

--- scripts/unlock-tf-state.sh
+++ scripts/unlock-tf-state.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 set -e
 
-component=$1
-storage_account_name=$2
+terraform_state_file=$1
+state_file_container_name=$2
+storage_account_name=$3
 
-echo "component is: $component"
-echo "storage_account_name is: $storage_account_name"
-
-leaseExist=`az storage blob show --container-name tfstate --name $component.tfstate --account-name $storage_account_name | jq -r '.properties.lease.state'`
+leaseExist=`az storage blob show --container-name $state_file_container_name --name $terraform_state_file --account-name $storage_account_name | jq -r '.properties.lease.state'`
 
 if [ ${leaseExist} = "leased" ]; then
-az storage blob lease break --blob-name $component.tfstate --container-name tfstate --account-name $storage_account_name
+az storage blob lease break --blob-name $terraform_state_file --container-name $state_file_container_name --account-name $storage_account_name
 else
 exit 0
 fi
