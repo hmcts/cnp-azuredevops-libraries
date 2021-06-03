f22571b38ca971c02ee47723161ae96a9cc19b67
Felix Eyetan <peyetan@yahoo.com>
2021-06-03 16:55:14 +0100

    Minor updates to build script name

--- scripts/unlock-tf-state.sh
+++ scripts/unlock-tf-state.sh
@@ -7,6 +7,8 @@ storage_account_name=$3
 
 leaseExist=`az storage blob show --container-name $state_file_container_name --name $terraform_state_file --account-name $storage_account_name | jq -r '.properties.lease.state'`
 
+echo "Current lease state: $leaseExist"
+
 if [ ${leaseExist} = "leased" ]; then
 az storage blob lease break --blob-name $terraform_state_file --container-name $state_file_container_name --account-name $storage_account_name
 else
