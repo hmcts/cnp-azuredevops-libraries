e2aa1d08335c2c7d062d8015b6f1110cdbe4c84b
Tim Jacomb <tim.jacomb@hmcts.net>
2021-06-04 15:41:13 +0100

    DTSPO-3239 Fix state unlock command

--- scripts/unlock-tf-state.sh
+++ scripts/unlock-tf-state.sh
@@ -4,10 +4,10 @@ set -e
 component=$1
 storage_account_name=$2
 
-leaseExist=`az storage blob show --container-name tfstate --name $component.tfstate --account-name $storage_account_name | jq -r '.properties.lease.state'`
+leaseExist=`az storage blob show --container-name subscription-tfstate --name "${component}" --account-name $storage_account_name | jq -r '.properties.lease.state'`
 
 if [ ${leaseExist} = "leased" ]; then
-az storage blob lease break --blob-name $component.tfstate --container-name tfstate --account-name $storage_account_name
+  az storage blob lease break --blob-name "${component}" --container-name subscription-tfstate --account-name $storage_account_name
 else
 exit 0
 fi
