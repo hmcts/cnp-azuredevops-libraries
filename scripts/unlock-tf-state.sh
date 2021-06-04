d8523bf92148e762a2dd2a239692385c2b056e96
Tim Jacomb <tim.jacomb@hmcts.net>
2021-06-04 15:44:42 +0100

    Update parameters

--- scripts/unlock-tf-state.sh
+++ scripts/unlock-tf-state.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-component=$1
-storage_account_name=$2
+storage_account_name=$1
+stateFilePath=$2
 
-leaseExist=`az storage blob show --container-name subscription-tfstate --name "${component}" --account-name $storage_account_name | jq -r '.properties.lease.state'`
+leaseExist=`az storage blob show --container-name subscription-tfstate --name "${stateFilePath}" --account-name $storage_account_name | jq -r '.properties.lease.state'`
 
 if [ ${leaseExist} = "leased" ]; then
-  az storage blob lease break --blob-name "${component}" --container-name subscription-tfstate --account-name $storage_account_name
+  az storage blob lease break --blob-name "${stateFilePath}" --container-name subscription-tfstate --account-name $storage_account_name
 else
 exit 0
 fi
