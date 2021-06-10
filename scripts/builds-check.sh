cc84d49686d4677e096fa764150aa942161091d1
mokainos <72137340+mokainos@users.noreply.github.com>
2021-06-10 19:32:02 +0100

    logging

--- scripts/builds-check.sh
+++ scripts/builds-check.sh
@@ -5,9 +5,10 @@ project="$SYSTEM_TEAMPROJECT"
 
 echo "This is build $thisbuild"
 echo "project is $SYSTEM_TEAMPROJECT"
-echo "https://dev.azure.com/$organization/$project/_apis/build/builds?api-version=5.1&definitions=$pipelinedefinition"
+link="https://dev.azure.com/$organization/$project/_apis/build/builds?api-version=5.1&definitions=$pipelinedefinition"
+echo "str_replace ( ' ', '%20', $link)"
 IFS=$'\n'
-JSON_DATA=($(curl -s -u :"$azuredevopstoken" --request GET "https://dev.azure.com/hmcts/'Shared Services'/_apis/build/builds?api-version=5.1&definitions=$pipelinedefinition" -H "Content-Type: application/json" | jq  '.value[] | .status + (.id|tostring)' | sort -u | grep inProgress))
+JSON_DATA=($(curl -s -u :"$azuredevopstoken" --request GET "https://dev.azure.com/hmcts/Shared Services/_apis/build/builds?api-version=5.1&definitions=$pipelinedefinition" -H "Content-Type: application/json" | jq  '.value[] | .status + (.id|tostring)' | sort -u | grep inProgress))
 buildnumber=(${JSON_DATA//[!0-9]/})
 
 
