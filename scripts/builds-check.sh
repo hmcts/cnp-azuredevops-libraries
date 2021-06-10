5db2ce4dd9b2c6657b257ddb2395a918b7b68e16
mokainos <72137340+mokainos@users.noreply.github.com>
2021-06-10 18:35:47 +0100

    logging

--- scripts/builds-check.sh
+++ scripts/builds-check.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-set -e
+set -ex
 organization="hmcts"
 project="$SYSTEM_TEAMPROJECT"
 
 echo "This is build $thisbuild"
+echo "project is $SYSTEM_TEAMPROJECT"
 IFS=$'\n'
 JSON_DATA=($(curl -s -u :"$azuredevopstoken" --request GET "https://dev.azure.com/$organization/$project/_apis/build/builds?api-version=5.1&definitions=$pipelinedefinition" -H "Content-Type: application/json" | jq  '.value[] | .status + (.id|tostring)' | sort -u | grep inProgress))
 buildnumber=(${JSON_DATA//[!0-9]/})
