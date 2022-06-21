cf6aa8bcabd991d746406a13331c85af35f8a7de
Tim Jacomb <21194782+timja@users.noreply.github.com>
2022-06-21 11:40:25 +0100

    DTSPO-8338 Extract container name to var

--- scripts/unlock-tf-state.sh
+++ scripts/unlock-tf-state.sh
@@ -4,15 +4,16 @@ set -e
 storage_account_name=$1
 stateFilePath=$2
 subscription=${3:-HMCTS-CONTROL}
+container=${4:-subscription-tfstate}
 
 az account set --subscription $subscription
 
 export AZURE_STORAGE_KEY=$(az storage account keys list  -n $storage_account_name --query [0].value -o tsv)
 
-leaseExist=`az storage blob show --container-name subscription-tfstate --name "${stateFilePath}" --account-name $storage_account_name | jq -r '.properties.lease.state'`
+leaseExist=`az storage blob show --container-name ${container} --name "${stateFilePath}" --account-name $storage_account_name | jq -r '.properties.lease.state'`
 
 if [ ${leaseExist} = "leased" ]; then
-  az storage blob lease break --blob-name "${stateFilePath}" --container-name subscription-tfstate --account-name $storage_account_name
+  az storage blob lease break --blob-name "${stateFilePath}" --container-name ${container} --account-name $storage_account_name
 else
 exit 0
 fi
