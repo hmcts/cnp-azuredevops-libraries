1713bad67751eabd8f74d39711d38a22237af40b
Zoe Cope <zoe.cope@hmcts.net>
2022-07-14 20:36:42 +0100

    try

--- scripts/add-pr-comment.ps1
+++ scripts/add-pr-comment.ps1
@@ -125,8 +125,7 @@ function Get-PlanBody {
         }
     }
     else {
-        $body = @{"body" = $("$planCommentPrefix had no plan`nSomething has gone wrong see: https://dev.azure.com//hmcts/CNP/_build/results?buildId={0}&view=charleszipp.azure-pipelines-tasks-terraform.azure-pipelines-tasks-terraform-plan" -f $buildId) }
-        Write-Host "The inputfile is empty, i.e. no plan so linking to task."
+        return $body
     }
 
     return $body
