c85a7d1185212d1b87741804cad524f969605483
Zoe Cope <zoe.cope@hmcts.net>
2022-07-15 11:30:36 +0100

    remove checkov

--- scripts/add-pr-comment.ps1
+++ scripts/add-pr-comment.ps1
@@ -86,8 +86,8 @@ function Minimize-Comment {
     #TODO: should really separate out the getting of data from the actual mimimization as it would allow easy unit testing.
     $comments.data.repository.pullRequest.comments.edges.node | Where-Object { $_.isMinimized -eq $false -and $_.body -match $matchingString -and $_.author.login -eq $author } | ForEach-Object {
         $body = "{`"query`":`"mutation (`$id: String)  {`\n  minimizeComment(input:{subjectId: `$id, clientMutationId:`\`"$((53..79) + (86..126) | Get-Random -Count 5 | ForEach-Object {[char]$_})`\`",classifier:DUPLICATE}){`\nminimizedComment{isMinimized}`\n  }`\n  `\n}`",`"variables`":{`"id`":`"$($_.id)`"}}" ;
-        if ($_.body.Length -gt 2) { $shortComment = $_.body.Substring(0, 2) }else { $shortComment = $_.body }
-        Write-Host "Minimizing Comment: $($_.id) for StageName: $stageName with Body (2 first chars):$shortComment.";
+        if ($_.body.Length -gt 20) { $shortComment = $_.body.Substring(0, 20) }else { $shortComment = $_.body }
+        Write-Host "Minimizing Comment: $($_.id) for StageName: $stageName with Body (20 first chars):$shortComment.";
         Invoke-RestMethod -Method Post -Uri $uri -Headers $headers -Body $body
     }
 
