20aa14379c8e0cb9ba50555dcebb75fdcd0c2a7f
Zoe Cope <zoe.cope@hmcts.net>
2022-07-18 11:08:57 +0100

    amend

--- scripts/install-tfcmt.sh
+++ scripts/install-tfcmt.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-BASE_URL=https://github.com/suzuki-shunsuke/tfcmt/releases/download
-DOWNLOAD_URL="${BASE_URL}/v${TFCMT_VERSION}/tfcmt_linux_amd64.tar.gz"
-TFCMT_VERSION=3.2.1
-wget ${DOWNLOAD_URL} -P /tmp
-tar zxvf /tmp/tfcmt_linux_amd64.tar.gz -C /tmp
-mv /tmp/tfcmt /usr/local/bin/tfcmt
+TFCMT_VERSION=v3.2.1
+
+curl -fL -o tfcmt.tar.gz https://github.com/suzuki-shunsuke/tfcmt/releases/download/$TFCMT_VERSION/tfcmt_linux_amd64.tar.gz
+tar -C /usr/bin -xzf ./tfcmt.tar.gz
