aa2401b51f0bf61a04fe958add0df7bfe0a3b84f
Zoe Cope <zoe.cope@hmcts.net>
2022-07-18 20:41:35 +0100

    for real

--- scripts/install-github-comment.sh
+++ scripts/install-github-comment.sh
@@ -2,5 +2,5 @@
 
 GH_COMMENT_VERSION=v4.2.0
 
-curl -fL -o tfcmt.tar.gz https://github.com/suzuki-shunsuke/tfcmt/releases/download/$GH_COMMENT_VERSION/github-comment_4.2.0_linux_amd64.tar.gz
+curl -fL -o tfcmt.tar.gz https://github.com/suzuki-shunsuke/github-comment/releases/download/$GH_COMMENT_VERSION/github-comment_4.2.0_linux_amd64.tar.gz
 tar -C /usr/bin -xzf ./tfcmt.tar.gz
