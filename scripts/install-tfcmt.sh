0091790b540a32c5c641d3c64c3d5f449d7ee4c0
Tim Jacomb <tim.jacomb@hmcts.net>
2022-07-19 11:20:51 +0100

    Use sudo

--- scripts/install-tfcmt.sh
+++ scripts/install-tfcmt.sh
@@ -3,4 +3,4 @@
 TFCMT_VERSION=v3.2.1
 
 curl -fL -o tfcmt.tar.gz https://github.com/suzuki-shunsuke/tfcmt/releases/download/$TFCMT_VERSION/tfcmt_linux_amd64.tar.gz
-tar -C /usr/bin -xzf ./tfcmt.tar.gz
+sudo tar -C /usr/bin -xzf ./tfcmt.tar.gz tfcmt
