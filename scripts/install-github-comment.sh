1315669f3a51c39cd667e9954260158ed92a6489
Zoe Cope <zoe.cope@hmcts.net>
2022-07-20 10:40:09 +0100

    number not id

--- scripts/install-github-comment.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-GH_COMMENT_VERSION=v4.2.0
-
-curl -fL -o tfcmt.tar.gz https://github.com/suzuki-shunsuke/github-comment/releases/download/$GH_COMMENT_VERSION/github-comment_4.2.0_linux_amd64.tar.gz
-tar -C /usr/bin -xzf ./tfcmt.tar.gz
