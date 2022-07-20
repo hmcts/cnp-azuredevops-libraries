5beaf47ec4c6d5ed7a5e5df1e2ccf8f631c75e6a
Zoe Cope <zoe.cope@hmcts.net>
2022-07-20 12:00:50 +0100

    as env var

--- scripts/install-tfcmt.sh
+++ scripts/install-tfcmt.sh
@@ -4,5 +4,3 @@ TFCMT_VERSION=v3.2.1
 
 curl -fL -o tfcmt.tar.gz https://github.com/suzuki-shunsuke/tfcmt/releases/download/$TFCMT_VERSION/tfcmt_linux_amd64.tar.gz
 sudo tar -C /usr/bin -xzf ./tfcmt.tar.gz tfcmt
-
-buildRepo=$(echo "$Build.Repository.Name" | cut -d '/' -f 2)
