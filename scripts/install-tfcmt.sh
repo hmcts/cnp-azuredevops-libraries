44cfc1e0cae68986ad1dab017a08be53e2e5ae38
Tim Jacomb <tim.jacomb@hmcts.net>
2022-07-25 15:33:27 +0100

    If no sudo then try install without

--- scripts/install-tfcmt.sh
+++ scripts/install-tfcmt.sh
@@ -3,4 +3,10 @@
 TFCMT_VERSION=v3.2.1
 
 curl -fL -o tfcmt.tar.gz https://github.com/suzuki-shunsuke/tfcmt/releases/download/$TFCMT_VERSION/tfcmt_linux_amd64.tar.gz
-sudo tar -C /usr/bin -xzf ./tfcmt.tar.gz tfcmt
+
+if command -v sudo &> /dev/null
+then
+    sudo tar -C /usr/bin -xzf ./tfcmt.tar.gz tfcmt
+else
+  tar -C /usr/bin -xzf ./tfcmt.tar.gz tfcmt
+fi
