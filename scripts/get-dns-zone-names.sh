9fa63a695bd069e4084cfd5b233db4ec914c0f3d
Enda Kelly <enda.kelly@hmcts.net>
2022-08-16 11:54:54 +0100

    use dedicated var for dns env

--- scripts/get-dns-zone-names.sh
+++ scripts/get-dns-zone-names.sh
@@ -2,24 +2,24 @@
 set -e
 
 if [ $ENVIRONMENT = 'sbox' ]; then
-    ENVIRONMENT=sandbox
+    DNS_ENVIRONMENT=sandbox
 fi
 
 if [ $PRODUCT = 'cft-platform' ]; then
     if [ $ENVIRONMENT = 'stg' ]; then
-        ENVIRONMENT=aat
+        DNS_ENVIRONMENT=aat
     elif [ $ENVIRONMENT = 'test' ]; then
-        ENVIRONMENT=perftest
+        DNS_ENVIRONMENT=perftest
     fi
     APP_NAME=plum
 fi
 
 if [ $PRODUCT = 'sds-platform' ]; then
     if [ $ENVIRONMENT = 'stg' ]; then
-        ENVIRONMENT=staging
+        DNS_ENVIRONMENT=staging
     fi
     APP_NAME=toffee
 fi
 
-echo "##vso[task.setvariable variable=ENVIRONMENT;isOutput=true]true"
+echo "##vso[task.setvariable variable=DNS_ENVIRONMENT;isOutput=true]true"
 echo "##vso[task.setvariable variable=APP_NAME;isOutput=true]true"
