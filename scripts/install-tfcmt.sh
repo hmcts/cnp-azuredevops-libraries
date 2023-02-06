5af9e395b90570e09dbfb5ce59322e071b9d3d20
hmcts-dependency-updater <104018155+hmcts-dependency-updater[bot]@users.noreply.github.com>
2023-02-06 02:21:06 +0000

    chore: Updated the content of the file "/tmp/updatecli/github/hmcts/c...
    ... np-azuredevops-libraries/scripts/install-tfcmt.sh"
    
    Made with ❤️️ by updatecli

--- scripts/install-tfcmt.sh
+++ scripts/install-tfcmt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TFCMT_VERSION=v4.0.1
+TFCMT_VERSION=v4.1.0
 
 curl -fL -o tfcmt.tar.gz https://github.com/suzuki-shunsuke/tfcmt/releases/download/$TFCMT_VERSION/tfcmt_linux_amd64.tar.gz
 
