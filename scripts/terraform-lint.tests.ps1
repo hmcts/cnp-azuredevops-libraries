5c52aa51f4918354b471b4163be06260b3e02d20
Rees Pozzi <rees.pozzi@justice.gov.uk>
2023-05-12 15:44:23 +0100

    Update scripts/terraform-lint.tests.ps1
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>

--- scripts/terraform-lint.tests.ps1
+++ scripts/terraform-lint.tests.ps1
@@ -22,7 +22,7 @@ else
     $TfFolderTestCases=@()
     ((($TfFiles).DirectoryName | Select-Object -Unique)).ForEach{$TfFolderTestCases += @{Instance = $_}}
       Context "Are correctly formatted" {
-        It "Terraform fmt command has been run on all files in <Instance>, to rewrite files to a canonical format and style." -TestCases $TfFolderTestCases {
+        It "All files in <Instance> are correctly formatted. By running 'terraform fmt'" -TestCases $TfFolderTestCases {
           Param($Instance)
           Invoke-Expression "terraform fmt -check=true $Instance"  | should -BeNullOrEmpty
       }
