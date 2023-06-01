7f4792dd184fb437bb53b691c19671b7026201c1
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-06-01 13:32:28 +0100

    added skipping publisher check

--- scripts/pester-tests.ps1
+++ scripts/pester-tests.ps1
@@ -1,3 +1,3 @@
 Get-Module -ListAvailable Pester
-# Install-Module -Name Pester -MaximumVersion 5.4.0 -Force -Verbose -Scope CurrentUser
+Install-Module -Name Pester -MaximumVersion 5.4.0 -Force -Verbose -Scope CurrentUser -SkipPublisherCheck
 Invoke-Pester ../ -OutputFormat NUnitXml -OutputFile ./TEST-CI.xml -EnableExit
