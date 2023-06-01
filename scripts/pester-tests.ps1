a613859c4538b5baa1930d1aae04ec4a9f8ba965
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-06-01 13:08:27 +0100

    testing to fix the pester module

--- scripts/pester-tests.ps1
+++ scripts/pester-tests.ps1
@@ -1,2 +1,4 @@
+Get-PSRepository
+Register-PSRepository -Name “PSGallery” -SourceLocation "https://www.powershellgallery.com/api/v2/" -InstallationPolicy Trusted
 Install-Module -Name Pester -MaximumVersion 5.4.1 -Force -Verbose -Scope CurrentUser
 Invoke-Pester ../ -OutputFormat NUnitXml -OutputFile ./TEST-CI.xml -EnableExit
