6de11320cea194e664614da11a1f1cc6a7e27d2f
Chirag Pareek <111435448+cpareek@users.noreply.github.com>
2023-06-01 14:25:58 +0100

    Update scripts/pester-tests.ps1
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>

--- scripts/pester-tests.ps1
+++ scripts/pester-tests.ps1
@@ -1,4 +1,1 @@
-Get-Module -ListAvailable Pester
-# Pester module seems to have come pre-installed with PowerShell@2 task and installing again may be creating a conflict hence commenting it out
-# Install-Module -Name Pester -MaximumVersion 5.4.1 -Force -Verbose -Scope CurrentUser
 Invoke-Pester ../ -OutputFormat NUnitXml -OutputFile ./TEST-CI.xml -EnableExit
