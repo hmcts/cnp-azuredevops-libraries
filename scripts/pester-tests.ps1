bf853c9205708987a43efedc08575c4024a8d1dd
Thomas Thornton <t.thornton@kainos.com>
2023-06-01 10:46:04 +0100

    Update pester-tests.ps1

--- scripts/pester-tests.ps1
+++ scripts/pester-tests.ps1
@@ -1,4 +1,2 @@
-Unregister-PSRepository -Name "PSGallery"
-Register-PSRepository -Default
-Install-Module -Name Pester -MaximumVersion 5.4.1 -Force -Verbose -Scope CurrentUser
+Install-Module -Name Pester -MaximumVersion 5.4.0 -Force -Verbose -Scope CurrentUser
 Invoke-Pester ../ -OutputFormat NUnitXml -OutputFile ./TEST-CI.xml -EnableExit
