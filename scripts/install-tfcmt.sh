6566d18aaff1b61470e2f69b64d0cae6352fde3c
hmcts-dependency-updater <104018155+hmcts-dependency-updater[bot]@users.noreply.github.com>
2023-06-26 02:30:19 +0000

    chore: Updated to content "TFCMT_VERSION=v4.4.2" in file "scripts/ins...
    ... tall-tfcmt.sh"
    
    Made with ❤️️ by updatecli

--- scripts/install-tfcmt.sh
+++ scripts/install-tfcmt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TFCMT_VERSION=v4.4.1
+TFCMT_VERSION=v4.4.2
 
 curl -fL -o tfcmt.tar.gz https://github.com/suzuki-shunsuke/tfcmt/releases/download/$TFCMT_VERSION/tfcmt_linux_amd64.tar.gz
 
