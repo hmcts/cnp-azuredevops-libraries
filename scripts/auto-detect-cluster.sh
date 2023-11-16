6c6386be29295addf88e37a63370981fe4de60b5
Marty Fox <martin.fox1@justice.gov.uk>
2023-11-16 15:55:24 +0000

    Updating script to add long options for inputs for better understanding of script

--- scripts/auto-detect-cluster.sh
+++ scripts/auto-detect-cluster.sh
@@ -1,59 +1,70 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-usage() {
-    echo "------------------------"
-    echo "Script to check which AKS cluster is active"
-    echo "------------------------"
-    echo "Usage: $0 -r <aksResourceGroupName> -k <aksClusterName> -e <aksEnvironmentName>"
-    echo "------------------------"
-    return 1
+set -euo pipefail
+
+resourceGroupName=
+aksClusterName=
+environmentName=
+
+usage(){
+>&2 cat << EOF
+------------------------------------------------
+Script to check if AKS cluster is active state
+------------------------------------------------
+Usage: $0
+    [ -r | --resourceGroupName ]
+    [ -a | --aksClusterName ]
+    [ -e | --environmentName ] 
+    [ -h | --help ] 
+EOF
+exit 1
 }
-OPTIND=1
 
-while getopts r:k:e: o; do
-    case "$o" in
-        r)
-            aksResourceGroupName="$OPTARG"
-            ;;
-        k)
-            aksClusterName="$OPTARG"
-            ;;
-        e)
-            aksEnvironmentName="$OPTARG"
-            ;;
-        --)
-            break
-            ;;
-        *)
-            echo "Unrecognized option '${flag}'"
-            usage
-            exit
-            ;;
+args=$(getopt -a -o r:a:e: --long resourceGroupName:,aksClusterName:,environmentName:,help -- "$@")
+if [[ $? -gt 0 ]]; then
+    usage
+fi
+
+# Debug commands, uncomment if you are having issues
+# >&2 echo [$@] passed to script
+# >&2 echo getopt creates [${args}]
+
+eval set -- ${args}
+while :
+do
+    case $1 in
+        -h | --help)               usage                  ; shift   ;;
+        -r | --resourceGroupName)  resourceGroupName=$2   ; shift 2 ;;
+        -a | --aksClusterName)     aksClusterName=$2      ; shift 2 ;;
+        -e | --environmentName)    environmentName=$2     ; shift 2 ;;
+        # -- means the end of the arguments; drop this, and break out of the while loop
+        --) shift; break ;;
+        *) >&2 echo Unsupported option: $1
+            usage ;;
     esac
 done
 
-
-if [ -z "$aksResourceGroupName" ] || [ -z "$aksClusterName" ] || [ -z "$aksEnvironmentName" ]; then
+if [ -z "$resourceGroupName" ] || [ -z "$aksClusterName" ] || [ -z "$environmentName" ]; then
         echo "------------------------" 
         echo 'Some values are missing, please supply AKS Resource Group, AKS Cluster name and AKS Environment name' >&2
         echo "------------------------"
         exit 1
 fi
 
-echo "aksResourceGroupName: $aksResourceGroupName";
-echo "aksClusterName: $aksClusterName";
-echo "aksEnvironmentName: $aksEnvironmentName";
+>&2 echo "resourceGroupName     : ${resourceGroupName}"
+>&2 echo "aksClusterName        : ${aksClusterName} "
+>&2 echo "environmentName       : ${environmentName}"
 
 check_pod_in_cluster() {
-    printf "\n\nTrying cluster $aksClusterName in $aksResourceGroupName\n"
+    printf "\n\nTrying cluster $aksClusterName in $resourceGroupName\n"
     az aks get-credentials \
-        --resource-group "$aksResourceGroupName" \
+        --resource-group "$resourceGroupName" \
         --name "$aksClusterName" --admin
-    local pod_exists
-    pod_exists=$(kubectl get pods --context "$aksClusterName-admin" -n admin -o jsonpath='{range .items[*]}{@.metadata.name}{"\t"}{@.spec.containers[0].args}{"\n"}{end}' | grep "external-dns-private" | grep -- "--txt-owner-id=$aksEnvironmentName-active")
+    local pod_exists=true
+    pod_exists=$(kubectl get pods --context "$aksClusterName-admin" -n admin -o jsonpath='{range .items[*]}{@.metadata.name}{"\t"}{@.spec.containers[0].args}{"\n"}{end}' | grep "external-dns-private" | grep -- "--txt-owner-id=$environmentName-active")
     if [ -n "$pod_exists" ]; then
-        aksResourceGroup="$aksResourceGroupName"
-        aksCluster="$aksClusterName"
+        setAksResourceGroup="$resourceGroupName"
+        setAksCluster="$aksClusterName"
         return 0
     else
         return 1
@@ -61,10 +72,10 @@ check_pod_in_cluster() {
 }
 
 if check_pod_in_cluster; then
-    echo "Pod with the specified argument found in cluster - $aksCluster."
+    echo "Pod with the specified argument found in cluster - $setAksCluster."
     echo "----------- Setting Pipeline Variables -----------------"
-    echo "##vso[task.setvariable variable=aksResourceGroup;isreadonly=true]$aksResourceGroup"
-    echo "##vso[task.setvariable variable=aksCluster;isreadonly=true]$aksCluster"
+    echo "##vso[task.setvariable variable=aksResourceGroup;isreadonly=true]$setAksResourceGroup"
+    echo "##vso[task.setvariable variable=aksCluster;isreadonly=true]$setAksCluster"
 else
     echo "Pod with the specified argument NOT found in cluster - $aksClusterName."
 fi
