01ffee06bb23c8da3d6c020167de885e1e93e55c
Daniel Kiriasidis Christou <daniel.christou@justice.gov.uk>
2023-11-16 15:32:40 +0000

    temporary change to test script

--- scripts/trigger-manual-deploy.sh
+++ scripts/trigger-manual-deploy.sh
@@ -1,21 +1,19 @@
-if kubectl config use-context cft-sbox-00-aks &> /dev/null; then
+#if kubectl config use-context cft-sbox-00-aks &> /dev/null; then
     echo "SBOX already running"
-else
+#else
     echo "Starting SBOX"
-    GITHUB_REPO="hmcts/auto-shutdown"
-    WORKFLOW_NAME="manual-start"
-
     # Trigger the workflow using the GitHub API
-    curl -X POST \
-      -H "Accept: application/vnd.github.v3+json" \
-      -H "Authorization: token $1" \
-      "https://api.github.com/repos/$GITHUB_REPO/actions/workflows/$WORKFLOW_NAME/dispatches" \
-      -d '{
-        "ref": "main",
-        "inputs": {
-          "Business area": "SDS",
-          "Environment": "sbox",
-          "Cluster": "All"
-        }
-      }'
-fi
+    curl -L \
+      -X POST \
+      -H "Accept: application/vnd.github+json" \
+      -H "Authorization: Bearer $1" \
+      -H "X-GitHub-Api-Version: 2022-11-28" \
+      https://api.github.com/repos/hmcts/auto-shutdown/actions/workflows/manual-start.yaml/dispatches \
+      -d '{ "ref": "master",
+                 "inputs": {
+                   "PROJECT": "SDS",
+                   "SELECTED_ENV": "sbox",
+                   "AKS-INSTANCES": "All"
+                 }
+               }'
+#fi
