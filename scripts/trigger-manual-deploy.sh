71526d255bb784bc0674663668d488cc79b93b09
Daniel Kiriasidis Christou <daniel.christou@justice.gov.uk>
2023-11-16 12:00:25 +0000

    Add step and token retrieval

--- scripts/trigger-manual-deploy.sh
+++ scripts/trigger-manual-deploy.sh
@@ -2,19 +2,20 @@ if kubectl config use-context cft-sbox-00-aks &> /dev/null; then
     echo "SBOX already running"
 else
     echo "Starting SBOX"
-    GITHUB_REPO="hmcts/aks-cft-deploy"
-    GITHUB_TOKEN="githubtoken" # Retrieve this via a azure cli command from azure keyvault
+    GITHUB_REPO="hmcts/auto-shutdown"
     WORKFLOW_NAME="manual-start"
 
     # Trigger the workflow using the GitHub API
     curl -X POST \
       -H "Accept: application/vnd.github.v3+json" \
-      -H "Authorization: token $GITHUB_TOKEN" \
+      -H "Authorization: token $1" \
       "https://api.github.com/repos/$GITHUB_REPO/actions/workflows/$WORKFLOW_NAME/dispatches" \
       -d '{
         "ref": "main",
         "inputs": {
+          "Business area": "SDS",
           "Environment": "sbox",
+          "Cluster": "All"
         }
       }'
 fi
