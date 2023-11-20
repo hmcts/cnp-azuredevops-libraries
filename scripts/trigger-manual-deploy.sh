0a11bedc5f93cc1b2aa30818e2e54eff97006247
Charles.Luo <charles.luo1@justice.gov.uk>
2023-11-20 17:48:37 +0000

    add a for loop to trigger auto shutdown workflows for supported projects and environments

--- scripts/trigger-manual-deploy.sh
+++ scripts/trigger-manual-deploy.sh
@@ -1,19 +1,28 @@
 #if kubectl config use-context cft-sbox-00-aks &> /dev/null; then
-    echo "SBOX already running"
+#    echo "SBOX already running"
 #else
-    echo "Starting SBOX"
+    echo "Starting Auto shutdown workflows ..."
+    PROJECTS=("SDS" "CFT")
+    ENVIRONMENTS=("sbox")
     # Trigger the workflow using the GitHub API
-    curl -L \
-      -X POST \
-      -H "Accept: application/vnd.github+json" \
-      -H "Authorization: Bearer $1" \
-      -H "X-GitHub-Api-Version: 2022-11-28" \
-      https://api.github.com/repos/hmcts/auto-shutdown/actions/workflows/manual-start.yaml/dispatches \
-      -d '{ "ref": "master",
-                 "inputs": {
-                   "PROJECT": "SDS",
-                   "SELECTED_ENV": "sbox",
-                   "AKS-INSTANCES": "All"
-                 }
+    for project in "${PROJECTS[@]}"; do
+      for env in "${ENVIRONMENTS[@]}"; do
+         echo "[info] triggering auto shutdown workflow for $project in $env ..."
+         curl -L \
+               -X POST \
+               -H "Accept: application/vnd.github+json" \
+               -H "Authorization: Bearer $1" \
+               -H "X-GitHub-Api-Version: 2022-11-28" \
+               https://api.github.com/repos/hmcts/auto-shutdown/actions/workflows/manual-start.yaml/dispatches \
+               -d '{ "ref": "master",
+                          "inputs": {
+                            "PROJECT": "$project",
+                            "SELECTED_ENV": "$env",
+                            "AKS-INSTANCES": "All"
+                          }
                }'
+
+      done
+    done
+    # Trigger the workflow using the GitHub API
 #fi
