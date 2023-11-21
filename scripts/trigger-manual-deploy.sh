ab310836418eca07463f65463db6ebbae6c54331
Charles.Luo <charles.luo1@justice.gov.uk>
2023-11-21 09:57:23 +0000

    use double quotes to expand expressions

--- scripts/trigger-manual-deploy.sh
+++ scripts/trigger-manual-deploy.sh
@@ -1,28 +1,27 @@
-#if kubectl config use-context cft-sbox-00-aks &> /dev/null; then
-#    echo "SBOX already running"
-#else
-    echo "Starting Auto shutdown workflows ..."
-    PROJECTS=("SDS" "CFT")
-    ENVIRONMENTS=("sbox")
-    # Trigger the workflow using the GitHub API
-    for project in "${PROJECTS[@]}"; do
-      for env in "${ENVIRONMENTS[@]}"; do
-         echo "[info] triggering auto shutdown workflow for $project in $env ..."
-         curl -L \
-               -X POST \
-               -H "Accept: application/vnd.github+json" \
-               -H "Authorization: Bearer $1" \
-               -H "X-GitHub-Api-Version: 2022-11-28" \
-               https://api.github.com/repos/hmcts/auto-shutdown/actions/workflows/manual-start.yaml/dispatches \
-               -d '{ "ref": "master",
-                          "inputs": {
-                            "PROJECT": $project,
-                            "SELECTED_ENV": $env,
-                            "AKS-INSTANCES": "All"
-                          }
-               }'
 
-      done
-    done
-    # Trigger the workflow using the GitHub API
-#fi
+echo "Starting Auto shutdown workflows ..."
+PROJECTS=("SDS" "CFT")
+ENVIRONMENTS=("sbox")
+# Trigger the workflow using the GitHub API
+for project in "${PROJECTS[@]}"; do
+  for env in "${ENVIRONMENTS[@]}"; do
+
+     #if kubectl config use-context cft-sbox-00-aks &> /dev/null; then
+     #else
+     #fi
+     echo "[info] triggering auto shutdown workflow for $project in $env ..."
+     curl -L \
+           -X POST \
+           -H "Accept: application/vnd.github+json" \
+           -H "Authorization: Bearer $1" \
+           -H "X-GitHub-Api-Version: 2022-11-28" \
+           https://api.github.com/repos/hmcts/auto-shutdown/actions/workflows/manual-start.yaml/dispatches \
+           -d "{ \"ref\": \"master\",
+                  \"inputs\": {
+                    \"PROJECT\": \"$project\",
+                    \"SELECTED_ENV\": \"$env\",
+                    \"AKS-INSTANCES\": \"All\"
+                  }
+                }"
+  done
+done
