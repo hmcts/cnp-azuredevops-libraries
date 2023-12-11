d90f7d8913c2f8c27c67fd9fb91753f2ffd70921
Rees Pozzi <reespozzi@icloud.com>
2023-12-11 12:49:23 +0000

    Only get status code from curl

--- scripts/trigger-manual-deploy.sh
+++ scripts/trigger-manual-deploy.sh
@@ -48,7 +48,7 @@ function start_unhealthy_environments() {
   while (( attempts <= MAX_ATTEMPTS ))
   do
     echo "Attempt #$attempts"
-    response=$(curl -sk -w "%{http_code}" "$TEST_URL")
+    response=$(curl -sk -o /dev/null -w "%{http_code}" "$TEST_URL")
     ((attempts++))
     if (( response >= 200 && response <= 399 )); then
       healthy=true;
