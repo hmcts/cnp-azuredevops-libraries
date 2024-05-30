984dbd7b8606f0e77e2fb14205213a93015e3234
Joshua Glass <joshua.glass@justice.gov.uk>
2024-05-30 15:20:31 +0100

    fixed array

--- scripts/ado-terraform-nagger.py
+++ scripts/ado-terraform-nagger.py
@@ -506,8 +506,8 @@ def main():
             error_warning, error_message = terraform_version_checker(terraform_version, config, current_date)
 
             if error_warning is True:
-                output_array[component]['severity'] = ['warning']
-                output_array[component]['message'] = [error_message]
+                output_array[component] = {'severity': error_warning}
+                output_array[component] = {'error_message': error_message}
 
             # Write the updated data back to the file
             with open(output_file, 'w') as file:
