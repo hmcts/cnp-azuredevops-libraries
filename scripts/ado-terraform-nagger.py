31e46c86acbfe3f70414e56f1d7a7dd3e848cc07
Joshua Glass <joshua.glass@justice.gov.uk>
2024-05-30 15:49:45 +0100

    updated msg ouput

--- scripts/ado-terraform-nagger.py
+++ scripts/ado-terraform-nagger.py
@@ -387,7 +387,7 @@ def terraform_version_checker(terraform_version, config, current_date):
         #     f'is lower than {config["terraform"]["terraform"]["version"]}. '
         #     f"Please upgrade before deprecation deadline {end_support_date_str}...",
         # )
-        return "warning", f'Terraform version {terraform_version} is lower than {config["terraform"]["terraform"]["version"]}. Please upgrade before deprecation deadline {end_support_date_str}.'
+        return True, f'Terraform version {terraform_version} is lower than {config["terraform"]["terraform"]["version"]}. Please upgrade before deprecation deadline {end_support_date_str}.'
 
     # Error if terraform version lower than specified & passed deadline.
     if version.parse(terraform_version) < version.parse(
