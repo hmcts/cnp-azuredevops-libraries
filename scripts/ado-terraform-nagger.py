6eeebba19531ac1cccc1a56361ba1be8db2010b6
Joshua Glass <joshua.glass@justice.gov.uk>
2024-05-30 12:04:14 +0100

    testing changes to array

--- scripts/ado-terraform-nagger.py
+++ scripts/ado-terraform-nagger.py
@@ -506,7 +506,7 @@ def main():
                 output_array = {}
 
             # Append warning/error if flagged
-            output_array[component].append({ "terraform_message": (terraform_version_checker(terraform_version, config, current_date)) })
+            output_array[component] = [{ "terraform_message": (terraform_version_checker(terraform_version, config, current_date)) }]
             # debug
             print(output_array)
 
