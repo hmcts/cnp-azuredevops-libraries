8008553d6fdf493be03077fdc5b8ef6ae994be66
Joshua Glass <joshua.glass@justice.gov.uk>
2024-06-10 11:27:19 +0100

    test for loop assignment

--- scripts/ado-terraform-nagger.py
+++ scripts/ado-terraform-nagger.py
@@ -691,9 +691,9 @@ def main():
                     output_array['error']['terraform_provider']['error_message'] = error_message
                     output_array['error']['terraform_provider']['provider'].append(provider)
             
-            # Write the updated data back to the file
-            with open(output_file, 'w') as file:
-                json.dump(output_array, file, indent=4)
+                # Write the updated data back to the file
+                with open(output_file, 'w') as file:
+                    json.dump(output_array, file, indent=4)
 
         with open(output_file, 'r') as file:
             complete_file = json.load(file)
