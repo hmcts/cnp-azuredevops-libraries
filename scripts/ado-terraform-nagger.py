483fd7693c562ade7b041fd58560b122e4d3121d
Joshua Glass <joshua.glass@justice.gov.uk>
2024-06-11 16:34:44 +0100

    typo

--- scripts/ado-terraform-nagger.py
+++ scripts/ado-terraform-nagger.py
@@ -197,7 +197,7 @@ def send_slack_message(webhook, channel, username, icon_emoji, build_origin, bui
     if message['warning']['terraform_provider']['provider']:
         providers_info = [
             f"{provider}: {version}" 
-            for provider, version in message['error']['terraform_provider']['provider'].items()
+            for provider, version in message['warning']['terraform_provider']['provider'].items()
         ]
         # Add the warning message block
         slack_data["blocks"].extend([
