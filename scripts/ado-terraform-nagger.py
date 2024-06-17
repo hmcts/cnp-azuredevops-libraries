fbea33f065efcbf120e093ad09a48eebd3918822
Joshua Glass <joshua.glass@justice.gov.uk>
2024-06-17 13:54:58 +0100

    change provider condition

--- scripts/ado-terraform-nagger.py
+++ scripts/ado-terraform-nagger.py
@@ -427,7 +427,7 @@ def terraform_provider_checker(provider, provider_version, config, current_date)
             "Please add it to the config in this file in order to "
             "compare it's versions.",
         )
-        return 'warning', f"Provider {provider} is missing from version config. Please add it to the config in this file in order to compare it's versions.", ''
+        return True, f"Provider {provider} is missing from version config. Please add it to the config in this file in order to compare it's versions.", ''
     else:
         # Handle providers
         # Get the date after which Terraform versions are no longer supported
