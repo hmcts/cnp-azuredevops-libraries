8beaf8f1a8132bc3ad33bf5e003c3fa1c508ac7e
Joshua Glass <joshua.glass@justice.gov.uk>
2024-06-21 11:45:53 +0100

    update error handling logic

--- scripts/ado-terraform-nagger.py
+++ scripts/ado-terraform-nagger.py
@@ -658,7 +658,7 @@ def main():
         with open(output_file, 'r') as file:
             complete_file = json.load(file)
             if (output_warning['terraform_version']['error_message'] or
-                output_warning['terraform_provider']['error_message'] and not output_warning['error']):
+                output_warning['terraform_provider']['error_message'] and not errors_detected):
                 print(f'complete file: { json.dumps(complete_file, indent=4, sort_keys=True) }')
                 log_message_slack(
                     slack_user_id,
@@ -702,7 +702,7 @@ def main():
         with open(output_file, 'r') as file:
             complete_file = json.load(file)
             if (output_warning['terraform_version']['error_message'] or
-                output_warning['terraform_provider']['error_message'] or output_warning['error']):
+                output_warning['terraform_provider']['error_message']):
                 print(f'error detected complete file: { json.dumps(complete_file, indent=4, sort_keys=True) }')
                 log_message_slack(
                     slack_user_id,
