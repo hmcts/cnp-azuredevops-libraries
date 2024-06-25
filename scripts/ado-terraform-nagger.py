e707381fccfe05b8ff7e865bdc8e30bcb301d7e6
Joshua Glass <joshua.glass@justice.gov.uk>
2024-06-25 15:53:39 +0100

    undo warning func changes

--- scripts/ado-terraform-nagger.py
+++ scripts/ado-terraform-nagger.py
@@ -539,18 +539,19 @@ def create_working_dir_list(base_directory, system_default_working_directory, bu
     return working_directory, components_list
 
 
-def add_error(output_warning, error_message, component):
-    # Create the 'error' key if it doesn't exist
-    if 'error' not in output_warning:
-        output_warning['error'] = {
-            'terraform_version': {
-                'components': [],
-                'error_message': ''
+def add_error(warning, output_warning, error_message, component):
+    if warning == 'error':
+        # Create the 'error' key if it doesn't exist
+        if 'error' not in output_warning:
+            output_warning['error'] = {
+                'terraform_version': {
+                    'components': [],
+                    'error_message': ''
+                }
             }
-        }
-    # Add the error message and component
-    output_warning['error']['terraform_version']['error_message'] = error_message
-    output_warning['error']['terraform_version']['components'].append(component)
+        # Add the error message and component
+        output_warning['error']['terraform_version']['error_message'] = error_message
+        output_warning['error']['terraform_version']['components'].append(component)
 
 
 def main():
@@ -634,8 +635,7 @@ def main():
                 output_warning['terraform_version']['error_message'] = error_message
                 output_warning['terraform_version']['components'].append(component)
 
-            elif warning == 'error':
-                add_error(output_warning, error_message, component)
+            add_error(warning, output_warning, error_message, component)
 
             # Handle providers
             terraform_providers = result["provider_selections"]
@@ -683,9 +683,8 @@ def main():
 
         # Handle terraform versions
         warning, error_message = terraform_version_checker(terraform_version, config, current_date)
-
-        if warning == 'error':
-            add_error(output_warning, error_message, component)
+        
+        add_error(warning, output_warning, error_message, component)
             
         # Write the updated data back to the file
         with open(output_file, 'w') as file:
