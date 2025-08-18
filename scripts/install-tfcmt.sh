3bc5870e087097b3927a8b1a4c12254a607a00bf
hmcts-dependency-updater <104018155+hmcts-dependency-updater[bot]@users.noreply.github.com>
2025-08-18 02:38:14 +0000

    chore: Bump tfcmt Version
    Made with ❤️️ by updatecli

--- scripts/install-tfcmt.sh
+++ scripts/install-tfcmt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TFCMT_VERSION=v4.14.9
+TFCMT_VERSION=v4.14.10
 
 curl -fL -o tfcmt.tar.gz https://github.com/suzuki-shunsuke/tfcmt/releases/download/$TFCMT_VERSION/tfcmt_linux_amd64.tar.gz
 
