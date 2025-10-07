811c39eaad2eb9926478a602c9a4fed46b348646
Dawid Strozak <dawid.strozak@hmcts.net>
2025-10-07 17:27:49 +0100

    Replace GitHub PAT token authentication on AKS Manual Start Workflow script with GitHub App Installation Access Token auth This fixes the issue where the PAT token did not have permissions to perform workflow dispatch, new GitHub App has been granted appropriate permissions

--- scripts/trigger-manual-deploy.sh
+++ scripts/trigger-manual-deploy.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 echo "Starting Auto Manual Start workflows ..."
-github_token="$1"
-project="$2"
-environment="$3"
+# GitHub App authentication details needed to get an app installation token
+# https://docs.github.com/en/apps/creating-github-apps/authenticating-with-a-github-app/authenticating-as-a-github-app-installation#using-an-installation-access-token-to-authenticate-as-an-app-installation
+github_app_priv_key=$1
+github_app_id=$2
+github_app_installation_id=$3
+project="$4"
+environment="$5"
 on_demand_environments=("sbox")
 MODE="start"
+GITHUB_APP_INSTALLATION_ACCESS_TOKEN=""
 
 # Only run for currently approved on demand environments
 if [[ ! " ${on_demand_environments[@]} " =~ " ${environment}" ]]; then
@@ -18,7 +23,50 @@ if [[ $project == "SS" ]]; then
   project="SDS"
 fi
 
+b64enc() { 
+  openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n';
+}
+
+function get_access_token() {
+  now=$(date +%s)
+  iat=$((${now} - 60)) # Issues 60 seconds in the past
+  exp=$((${now} + 600)) # Expires 10 minutes in the future
+  
+  header_json='{
+      "typ":"JWT",
+      "alg":"RS256"
+  }'
+  # Header encode
+  header=$( echo -n "${header_json}" | b64enc )
+  payload_json="{
+      \"iat\":${iat},
+      \"exp\":${exp},
+      \"iss\":\"${github_app_id}\"
+  }"
+  # Payload encode
+  payload=$( echo -n "${payload_json}" | b64enc )
+  # Signature
+  header_payload="${header}"."${payload}"
+  signature=$(
+      openssl dgst -sha256 -sign <(echo -n "${github_app_priv_key}") \
+      <(echo -n "${header_payload}") | b64enc
+  )
+
+  # Create JWT
+  JWT="${header_payload}"."${signature}"
+
+  # Get the installation access token - expires after 1 hour
+  curl --request POST \
+      --url https://api.github.com/app/installations/${github_app_installation_id}/access_tokens \
+      --header "Accept: application/vnd.github+json" \
+      --header "Authorization: Bearer ${JWT}" \
+      --header "X-GitHub-Api-Version: 2022-11-28" \
+      --silent | jq -r .token
+}
+
 function trigger_workflow() {
+  # Trigger the workflow dispatch event using dedicated GitHub App with action: write permission on auto-shutdown repo
+  # See: AKS Manual Start Workflow GitHub App in Platform Operations/HMCTS GitHub Apps
   curl -L \
          -X POST \
          -H "Accept: application/vnd.github+json" \
@@ -63,7 +111,11 @@ function check_environment_health() {
 
 # Function that will trigger workflow if environment is not up, to start it
 function start_unhealthy_environments() {
-  github_token="$1"
+  # Only generate the app installation access token if it doesn't already exist
+  if [[ -z "$GITHUB_APP_INSTALLATION_ACCESS_TOKEN" ]]; then
+    GITHUB_APP_INSTALLATION_ACCESS_TOKEN=$(get_access_toke)
+  fi
+
   project="$2"
   environment="$3"
 
@@ -71,8 +123,9 @@ function start_unhealthy_environments() {
     echo "$project in $environment is healthy, returned HTTP $response. No need to trigger auto manual start workflow."
   else
     echo "[info] $project in $environment not healthy, triggering auto manual start workflow for $project in $environment"
-    trigger_workflow "$github_token" "$MODE" "$project" "$environment"
+    trigger_workflow "${GITHUB_APP_INSTALLATION_ACCESS_TOKEN}" "$MODE" "$project" "$environment"
     echo "[info] Manual start workflow for $project in $environment triggered.. waiting 5 minutes for environment to start"
+
     # Wait 5 minutes for environment to start
     sleep 300
     MAX_ATTEMPTS=5
@@ -100,9 +153,9 @@ function start_unhealthy_environments() {
 
 if [[ $project == "PANORAMA" ]]; then
   echo "Triggering auto manual start workflow for all projects in $environment"
-  start_unhealthy_environments "$github_token" "SDS" "$environment"
-  start_unhealthy_environments "$github_token" "CFT" "$environment"
+  start_unhealthy_environments "SDS" "$environment"
+  start_unhealthy_environments "CFT" "$environment"
   exit 0
 fi
 
-start_unhealthy_environments "$github_token" "$project" "$environment"
+start_unhealthy_environments "$project" "$environment"
