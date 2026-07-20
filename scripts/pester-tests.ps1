8ece4ccc804027eccbd31659b05d0381a0fd2df4
hmcts-dependency-updater <104018155+hmcts-dependency-updater[bot]@users.noreply.github.com>
2026-07-20 02:59:42 +0000

    chore: Bump pester Version
    Made with ❤️️ by updatecli

--- scripts/pester-tests.ps1
+++ scripts/pester-tests.ps1
@@ -1,4 +1,4 @@
-$PESTER_VERSION=6.0.0
+$PESTER_VERSION=6.0.1
 
 if (-Not (Get-Module -ListAvailable -Name Pester)) {
   Install-Module -Name Pester -MaximumVersion $PESTER_VERSION -Force -Verbose -Scope CurrentUser
